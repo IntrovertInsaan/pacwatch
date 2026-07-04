c2b3029308d6780acc67c8944f7db01f9bd06366
raja <rajadodve43@gmail.com>
2026-07-04 05:51:34 +0530

    Add pacman module and Package struct

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+mod pacman;
+use pacman::Package;
+
 fn main() {
-    println!("Hello, world!");
+    let pkg = Package {
+        name: "bat".to_string(),
+        version: "0.26.1-1".to_string(),
+        description: "A cat clone with syntax highlighting".to_string(),
+    };
+    println!("{:?}", pkg);
 }
---
+++ src/pacman.rs
@@ -0,0 +1,6 @@
+#[derive(Debug, Clone, Default)]
+pub struct Package {
+    pub name: String,
+    pub version: String,
+    pub description: String,
+}
